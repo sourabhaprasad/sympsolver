c2218755fa765475227be2776af151e9ae9eaad1
Sourabha K H <sourabhaprasad04@gmail.com>
2025-01-22 23:54:06 +0530

    map

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,13 @@
 "use client";
-import Link from "@node_modules/next/link";
-import Image from "@node_modules/next/image";
-import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
-    <nav className="flex justify-between w-full mb-16 p-3  bg-[#EBDAD2]">
+    <nav className="flex justify-between w-full mb-16 p-3 bg-[#EBDAD2]">
       <Link href="/" className="flex gap-2 items-center">
         <Image
           src="/images/logo.png"
@@ -16,23 +18,22 @@ const Navbar = () => {
         />
         <p className="font-handlee text-3xl font-bold">SympSolver</p>
       </Link>
-
-      <div className="flex gap-4 items-center text-[20px] font-funnel ">
-        <Link href="/" className="link">
-          Home
-        </Link>
-        <Link href="/about" className="link">
-          About
-        </Link>
-        <Link href="/checker" className="link">
-          Checker
-        </Link>
-        <Link href="/chatbot" className="link">
-          ChatBot
-        </Link>
-        <Link href="/urgentcare" className="link">
-          Urgent-Care
-        </Link>
+      <div className="flex gap-4 items-center text-[20px] font-funnel">
+        {[
+          { name: "Home", href: "/" },
+          { name: "About", href: "/about" },
+          { name: "Checker", href: "/checker" },
+          { name: "ChatBot", href: "/chatbot" },
+          { name: "Urgent-Care", href: "/urgentcare" },
+        ].map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={`link ${pathname === link.href ? "text-blue-500" : ""}`}
+          >
+            {link.name}
+          </Link>
+        ))}
       </div>
     </nav>
   );
